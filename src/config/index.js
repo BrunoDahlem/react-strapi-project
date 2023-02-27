a45b21a25d692d7f42c47430e7ab248a13387efa
Bruno Dahlem <brunodahlempereira.1306@gmail.com>
2023-02-27 19:44:48 -0300

    Ajuste url da API

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export const config = {
-  url: 'http://192.168.100.243:1337/api/pages?populate=deep',
+  url: 'https://strapi.brunodahlem.com.br//api/pages?populate=deep',
   siteName: 'Landing Pages |',
   homePage: 'home',
 };
